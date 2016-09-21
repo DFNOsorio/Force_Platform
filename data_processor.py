6bab387ef45c18a4c4a952837df3ef1a527135f6
daniel_ubuntu <vdosavh@gmail.com>
2016-09-21 12:13:25 +0100

    a

--- data_processor.py
+++ data_processor.py
@@ -4,6 +4,7 @@ import numpy as np
 
 patient = 'Equilibrio/Paulo'
 
+
 EMG_data, EMG_time, EMG_labels, EMG_def, ForcePlatform_data, ForcePlatform_time, ForcePlatform_labels, ForcePlatform_def = load_data_h5(patient)
 
 EMG = [EMG_data, EMG_time, EMG_labels, EMG_def]
